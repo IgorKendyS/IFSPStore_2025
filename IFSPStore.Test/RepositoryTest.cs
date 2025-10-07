ffddc5b6851da8f57ca851411fcef83f656b01f7
IgorKendyS <igor-kendy@hotmail.com>
2025-10-07 11:40:28 -0300

    Test added data into database successfully

--- IFSPStore.Test/RepositoryTest.cs
+++ IFSPStore.Test/RepositoryTest.cs
@@ -1,5 +1,7 @@
-﻿using IFSPStore.Repository.Context;
+﻿using IFSPStore.Domain.Entities;
+using IFSPStore.Repository.Context;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace IFSPStore.Test
 {
@@ -12,6 +14,23 @@ namespace IFSPStore.Test
             using (var context = new IFSPStoreContext())
             {
                 Debug.WriteLine("Connected!");
+                var category1 = new Category(1, "Carnes");
+                var category2 = new Category(2, "Bebidas");
+                var category3 = new Category(3, "Doces");
+
+                context.Categories.Add(category1);
+                context.Categories.Add(category2);
+                context.Categories.Add(category3);
+                context.SaveChanges();
+                Console.WriteLine("Categories added!");
+            }
+            using (var context = new IFSPStoreContext())
+            {
+                foreach(var cat in context.Categories)
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(cat));
+                }
+                Console.WriteLine("List done!");
             }
         }
     }
